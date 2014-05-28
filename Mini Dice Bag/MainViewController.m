0ce863ec2ba2972cb49deaad5bf9e532e14ac519
Michael Enger <michaelenger@live.com>
2014-05-28 20:38:24 +0200

    Reorganize and rename just the way I like it

---
+++ Mini Dice Bag/MainViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Mini Dice Bag
+//
+//  Created by Michael Enger on 28/05/14.
+//  Copyright (c) 2014 Michael Enger. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+#pragma mark UIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
